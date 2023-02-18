e3126788b858f1fa08b97d0f4e2b230b56588577
Richard Oke <okerichard55@gmail.com>
2023-02-17 18:46:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- * Return 1 (Success)
- */
+ * Return: Always 1
+ *  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
