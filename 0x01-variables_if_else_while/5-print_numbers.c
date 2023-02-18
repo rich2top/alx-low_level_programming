f6852a1933a9ee57f142d59b29490e186c3444ba
Richard Oke <okerichard55@gmail.com>
2023-02-17 20:41:59 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Assign a random number to the variable n each time it is executed
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
