2dbc9b6d3725f7ca8e7f41414203869732577456
Richard Oke <okerichard55@gmail.com>
2023-03-20 17:06:20 -0700

    commit message for task 1 revised

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: owner to initialize
  */
 
-Void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
