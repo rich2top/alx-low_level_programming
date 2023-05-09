bd015a98c64cb29de4b7273592366f50b35b362c
Richard Oke <okerichard55@gmail.com>
2023-05-09 05:05:27 -0700

    file i/o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,9 @@
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+void print_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
